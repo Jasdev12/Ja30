c3c2530a81639046447e929dfc08c2e1af2997ab
jasdev12 <jaswanthdevireddy77@gmail.com>
2022-08-10 17:57:26 +0530

    function component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from './Components/Hello'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save.
-
-        </p>
-        <h1>Hell</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello/>
     </div>
   );
 }
